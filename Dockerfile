c04c880ebd4e27c0f813a43ad0c58c50f45471d7
Alon Noy <alon.noy@algosec.com>
2022-07-19 09:42:58 +0300

    Get changed files

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.10
 
 RUN curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
-RUN apt-get update && apt-get install -y jq curl
+RUN apt-get update && apt-get install -y jq curl git
 
 WORKDIR /app
 COPY main.sh /app/main.sh
--- main.sh
+++ main.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+
+################## Get Changed files ###############
+# Pull Request
+git fetch origin "${{ github.base_ref }}" --depth=1
+# Get the list of all changed resources
+diff_result=$(git diff --name-only "origin/${{ github.base_ref }}" ${{ github.sha }} )
+echo "Diff between origin/${{ github.base_ref }} and ${{ github.sha }}"
+# Extract terraform's files
+terraform_files=$(echo $diff_result | tr -s '[[:space:]]' '\n' | grep -o '.*\.tf$')
+echo "Changed Terraform's files: $terraform_files"
+#extract folders where the changed teraforms files are stored
+#and create json to proceed them in the matrix style
+matrix_output="{\"include\":[ "
+for line in in $terraform_files
+do 
+  if [[ $line == *".tf"* ]];
+  then
+    echo "Working line: $line" 
+    dir=$(dirname $line) 
+    echo "extracted dir: $dir"
+    matrix_output="$matrix_output{\"folder\":\"$dir\"},"
+  fi
+done
+matrix_output="$matrix_output ]}"
+echo "Prepared working matrix: $matrix_output"
+echo "::set-output name=matrix::${matrix_output}"
+
+
+################## END: Get Changed files ###############
+
 cd $WORKING_DIRECTORY
 
 TMP_DIR=${TMP_DIR:-/app}
